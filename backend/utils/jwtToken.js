ddcc2274b2c9d0e65b1b587c3bcc3f944192099b
sachin-beniwal <sachinbeniwal0066@gmail.com>
2024-12-12 12:26:23 +0530

    Update jwtToken.js

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -6,6 +6,7 @@ export const sendToken = (user, statusCode, res, message) => {
     ),
     httpOnly: true,
     secure: true,
+    sameSite: 'None'
   };
 
   res.status(statusCode).cookie("token", token, options).json({
